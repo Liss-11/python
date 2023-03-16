60635e2a963351347f69065d01b9e2f40a90f55c
Alissia Frolova <afrolova@cbr5s6.42barcelona.com>
2023-03-16 12:03:33 +0100

    9_done

--- ex09/guess.py
+++ ex09/guess.py
@@ -1,44 +1,28 @@
-import sys
 import random
-import string
 
 if __name__=='__main__':
     secret_num = random.randint(1, 99)
-    answer = -1
-    print(secret_num)
+    answer = ""
     attempts = 0
     print("This is an interactive guessing game!\nYou have to enter a number between 1 and 99 to find out the secret number.\nType 'exit' to end the game.\nGood luck!")
-    #answer = input("\nWhat's your guess between 1 and 99?\n")
-    while answer != str(secret_num):
+    while str(answer) != str(secret_num):
         attempts += 1
-        answer = raw_input("\nWhat's your guess between 1 and 99?\n")
-        print(answer)
-        if answer == 'exit':
-            print("Goodbye!")
-            exit(0)
-        if not isinstance(answer, int):
-            print("That's not a number.")
+        try: input = raw_input
+        except NameError: pass
+        answer = input("\nWhat's your guess between 1 and 99?\n")
+        try:
+            int(answer)
+            if int(answer) > secret_num:
+                print("Too high!")
+            elif int(answer) < secret_num:
+                print("Too low!")
+            else:
+                if secret_num == 42:
+                  print("The answer to the ultimate question of life, the universe and everything is 42.")
+        except:
             if answer == "exit":
-                print("IS a exit")
-            answer = -1
-            break
-        answer = int(answer)
-        if answer > secret_num:
-            print("Too high!")
-        elif answer < secret_num:
-            print("Too low!")
-        else:
-            if secret_num == 42:
-                print("The answer to the ultimate question of life, the universe and everything is 42.")            
-    print("Congratulations! You got it on your first try!" if attempts == 1 else "Congratulations, you've got it!\nYou won in"  + attempts + "attempts!")
+                exit(0) 
+            else:
+                print("That's not a number.")         
+    print("Congratulations! You got it on your first try!" if attempts == 1 else "Congratulations, you've got it!\nYou won in "  + str(attempts) + " attempts!")
     exit(0)
-
-    
-           
-            
-
-
-    #si el numero es 42 - poner la cita
-    #contar los intentos
-    #felicitacion si acierta a la primera
-    #indicar si ha dicho mas alto o mas bajo
